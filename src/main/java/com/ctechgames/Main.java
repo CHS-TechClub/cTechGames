353e3b471b4b66c4ce53ff9b9a1a59e254fe1ff1
Flaymed <markcviggiano@gmail.com>
2022-11-05 19:51:54 -0400

    Initial Plugin

---
+++ src/main/java/com/ctechgames/Main.java
@@ -0,0 +1,12 @@
+package com.ctechgames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    getLogger().info("Starting cTechGames!");
+  }
+
+}
